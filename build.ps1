98359cd2d26108738108ec760c1470562aae20e0
David Graham <dave@davegraham.org>
2017-02-17 20:54:26 -0700

    Add powershell build script for windows use
    Most of the ebook development occures on windows.  This script will
    build the epub files on windows.

---
+++ build.ps1
@@ -0,0 +1,26 @@
+Write-Output "Start Build Battle Gaming ebooks"
+
+$exists = Test-Path "publish"
+If (-Not $exists) {
+    New-Item -ItemType directory -Name "publish"
+}
+
+Write-Output "Building Warrior Code System Core Document"
+$exists = Test-Path "publish\Warrior-Code-System-Core-Document.epub"
+If ($exists) {
+    Remove-Item "publish\Warrior-Code-System-Core-Document.epub"
+}
+Compress-Archive -Path "ebooks\warrior-code\sys-core-doc" -DestinationPath "publish\Warrior-Code-System-Core-Document.zip"
+Rename-Item -Path "publish\Warrior-Code-System-Core-Document.zip" -NewName "Warrior-Code-System-Core-Document.epub"
+Write-Output "Built publish\Warrior-Code-System-Core-Document.epub"
+
+Write-Output "Building Warrior Code Padded Weapon Rules Document"
+$exists = Test-Path "publish\Warrior-Code-Padded-Weapon-Rules-Document.epub"
+If ($exists) {
+    Remove-Item "publish\Warrior-Code-Padded-Weapon-Rules-Document.epub"
+}
+Compress-Archive -Path "ebooks\warrior-code\padded-weapon-rules-doc" -DestinationPath "publish\Warrior-Code-Padded-Weapon-Rules-Document.zip"
+Rename-Item -Path "publish\Warrior-Code-Padded-Weapon-Rules-Document.zip" -NewName "Warrior-Code-Padded-Weapon-Rules-Document.epub"
+Write-Output "Built publish\Warrior-Code-Padded-Weapon-Rules-Document.epub"
+
+Write-Output "End Build Battle Gaming ebooks"
